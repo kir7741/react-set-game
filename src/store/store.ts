ca9b6d9dd0da70d75349c577395f687fd92ee051
Bo Ru Chien <andy511032002@gmail.com>
2023-05-29 22:55:37 +0800

    build: add redux-thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
 import { applyMiddleware, compose, createStore, Store } from "redux";
 import reducers, { GlobalState } from '../models/reducers';
 import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk-fsa';
 
 let composeEnhancers = compose;
-const middleware = [logger];
+const middleware = [logger, ReduxThunk];
 
 composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line @typescript-eslint/no-explicit-any
 
