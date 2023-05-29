edd6c22582ae8de07c577750a47f50bca732bac0
Bo Ru Chien <andy511032002@gmail.com>
2023-05-29 23:04:37 +0800

    build: add promiseMiddleWare

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import { applyMiddleware, compose, createStore, Store } from "redux";
 import reducers, { GlobalState } from '../models/reducers';
 import logger from 'redux-logger';
-import ReduxThunk from 'redux-thunk-fsa';
+import thunkMiddleware from 'redux-thunk-fsa';
+import promiseMiddleware from 'redux-promise-middleware';
 
 let composeEnhancers = compose;
-const middleware = [logger, ReduxThunk];
+const middleware = [logger, thunkMiddleware, promiseMiddleware];
 
 composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line @typescript-eslint/no-explicit-any
 
